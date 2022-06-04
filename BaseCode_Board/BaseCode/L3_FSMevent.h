e66e7e1537dba35ab5f00ed723d5bfb298322fbc
Hyunsun <gustjs1013@naver.com>
2022-06-04 16:10:20 +0900

    board
    board_codes

--- BaseCode_Board/BaseCode/L3_FSMevent.h
+++ BaseCode_Board/BaseCode/L3_FSMevent.h
@@ -1,7 +1,11 @@
 typedef enum L3_event
 {
-    L3_event_msgRcvd = 2,
-    L3_event_dataToSend = 4,
+    L3_event_reqToSend = 1, //A
+    L3_event_release =2, //B
+    L3_event_resRcvd = 3, //C Answer for Requst qualitification
+    L3_event_msgToSend = 4, //D
+    L3_event_msgEnd =5, //E message count>=10
+    
 } L3_event_e;
 
 
