72adfc145a92a8fbec21f379e8a0c8c990951a57
kyb722 <101792041+kyb722@users.noreply.github.com>
2022-06-09 17:44:56 +0900

    mo

--- BaseCode_Arb/BaseCode/L3_FSMmain.cpp
+++ BaseCode_Arb/BaseCode/L3_FSMmain.cpp
@@ -34,7 +34,7 @@ static void L3service_processInputWord(void)
         if (c == '\n' || c == '\r')
         {
             originalWord[wordLen++] = '\0';
-            if (originalWord == "REQ_Q"){
+            if (strncmp(originalWord,"REQ_Q",5) == 0){
                 L3_event_setEventFlag(L3_event_dataToSend);    //질문
             } else L3_event_setEventFlag(L3_event_msgToSend);  //질문
 
@@ -67,7 +67,7 @@ void L3_FSMrun(void)
 {   
     if (prev_state != main_state)
     {
-        debug_if(DBGMSG_L3, "[L3] State transition from %i to %i\n", prev_state, main_state);
+       //debug_if(DBGMSG_L3, "[L3] State transition from %i to %i\n", prev_state, main_state);
         prev_state = main_state;
     }
 
@@ -99,7 +99,8 @@ void L3_FSMrun(void)
                     //msg header setting
                     // if received message is "MSG_TYPE_QUA_REQ" --> change state to the "Communicate"
                     L3_msg_encodeData(sdu, 1); // permit value=1 , type--> MSG_TYPE_QUA_RES==1 
-                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)); 
+                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)/sizeof(char));
+
                     main_state = L3STATE_COMMUNICATE;
                     //timer start
                     L3_timer_startTimer();
@@ -124,24 +125,30 @@ void L3_FSMrun(void)
                 }
                 else if(dataPtr[0] == 2 && L3_event_checkEventFlag(L3_event_MSGRcvd)) { // MSG_TYPE_MSG_SEND && L3_event_MSGRcvd
                     memcpy(_msg, &dataPtr[L3_MSG_OFFSET_DATA], (size-1)*sizeof(char))
-                    //sdu = L3_msg_encodeData(_msg,sizeof(_msg)+1,2);  
+                    //sdu = L3_msg_encodeData(_msg,sizeof(_msg)+1,2);
+                    //질문 if pdu type = 2 && current_board == data sended borad's number->arb send data to group  
                     L3_msg_encodeMessage(sdu,originalWord,2);//send message
-                    L3_LLI_dataReqFunc(sdu, sizeof(sdu));
-                    //질문 if pdu type = 2 && current_board == data sended borad's number->arb send data to group
-                    // if arq_count>10(group is not exist or disconnected) -> act stop ->massage timer stop -> send PDU type = 6 to board 
-                    //  ->arb release
-                    main_state = L3STATE_COMMUNICATE;
+                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)/sizeof(char));
+
+                    
+                    // if arq_count>10(group is not exist or disconnected) -> act stop ->massage timer stop -> send PDU type = 6 to board ->arb release
+                    
+                    L3_timer_stopTimer(); //massage timer stop
+                    L3_msg_encodeData(sdu, 6); //send PDU type = 6 to board
+                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)/sizeof(char));
+
+                    main_state = L3STATE_COMMUNICATE; //arb release
                 }
                  else if(dataPtr[0] == 3 && L3_event_checkEventFlag(L3_event_RLSRcvd)) { // MSG_TYPE_RLS_REQ && L3_event_RLSRcvd
                     //sdu = L3_msg_encodeData(sdu,2, 4);
                     L3_msg_encodeData(sdu,4); //type-> MSG_TYPE_QUA_RLS==4
-                    L3_LLI_dataReqFunc(sdu, sizeof(sdu));
+                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)/sizeof(char));
                     
                     main_state = L3STATE_IDLE;
                 }
                  else if(L3_event_checkEventFlag(L3_event_Timeout)){
                     L3_msg_encodeData(sdu,5); //type-> MSG_TYPE_TIME_OUT==5
-                    L3_LLI_dataReqFunc(sdu, sizeof(sdu));
+                    L3_LLI_dataReqFunc(sdu, sizeof(sdu)/sizeof(char));
 
                     L3_event_clearEventFlag(L3_event_Timeout);
                     main_state = L3STATE_IDLE;
