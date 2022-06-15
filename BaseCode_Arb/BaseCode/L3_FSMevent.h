c250b8b2263160dec5ce942e49ad163faec1aaa3
Hyunsun <gustjs1013@naver.com>
2022-06-16 01:44:11 +0900

    edit Arb & board
    edit Arb & board

--- BaseCode_Arb/BaseCode/L3_FSMevent.h
+++ BaseCode_Arb/BaseCode/L3_FSMevent.h
@@ -1,13 +1,10 @@
 typedef enum L3_event
 {
     L3_event_reqRcvd =1, //A
-    L3_event_MSGRcvd =2, //B
-    L3_event_RLSRcvd =3, //C
+    L3_event_MsgRcvd =2, //B
+    L3_event_RlsRcvd =3, //C
     L3_event_Timeout =4, //D
-<<<<<<< Updated upstream
     
-=======
->>>>>>> Stashed changes
 } L3_event_e;
 
 void L3_event_setEventFlag(L3_event_e event);
--- BaseCode_Arb/BaseCode/L3_msg.cpp
+++ BaseCode_Arb/BaseCode/L3_msg.cpp
@@ -1,16 +1,10 @@
 #include "mbed.h"
 #include "L3_msg.h"
-<<<<<<< Updated upstream
-#include "mbed.h"
-#include "L3_msg.h"
-=======
->>>>>>> Stashed changes
 #include "string.h"
 
 
 /*
 #define MSG_TYPE_QUA_REQ 0
-<<<<<<< Updated upstream
 #define MSG_TYPE_QUA_RES 1 //발언권 승인(value = 1) 거절(value =0)
 #define MSG_TYPE_MSG_SEND 2
 #define MSG_TYPE_RLS_REQ 3
@@ -20,7 +14,6 @@
 #define MSG_TYPE_MSG_SEND 2
 #define MSG_TYPE_RLS_REQ 3
 #define MSG_TYPE_QUA_RLS 4
->>>>>>> Stashed changes
 #define MSG_TYPE_TIME_OUT 5
 #define MSG_TYPE_NO_GROUP 6
 */
@@ -52,11 +45,47 @@ char L3_msg_encodeData(char* data, int type)
     return *data;
 }
 
-char L3_msg_encodeData(char* data,char* msg_data, int type)
+
+char L3_msg_encodeQual(char* data, int type, int state){
+    data[L3_MSG_OFFSET_TYPE] = MSG_TYPE_QUA_RES;
+    if(state ==1) { //initial state, current board
+        memcpy(&data[L3_MSG_OFFSET_DATA],"1", sizeof(char));
+    } else{
+        memcpy(&data[L3_MSG_OFFSET_DATA],"0", sizeof(char));
+    }
+    return *data;
+}
+
+char L3_msg_encodeMessage(char* data,char* msg_data, int type)
 {
-    
     data[L3_MSG_OFFSET_TYPE] = MSG_TYPE_MSG_SEND;
     memcpy(&msg_data[L3_MSG_OFFSET_DATA], msg_data, sizeof(msg_data));
-
     return *data;
+}
+
+/*
+L3_event_reqRcvd =1, //A
+    L3_event_MsgRcvd =2, //B
+    L3_event_RlsRcvd =3, //C
+    L3_event_Timeout =4, //D
+    */
+L3_event_e L3_decode_EventFlag(char* data)
+{
+    L3_event_e event;
+    switch (data[0])
+    {
+        case '0': //requset qualitifiaction received
+            event = L3_event_reqRcvd;
+            break;
+        case '2': //requset qualitifiaction received
+            event = L3_event_MsgRcvd;
+            break;
+        case '3': //requset qualitifiaction received
+            event =L3_event_RlsRcvd;
+            break;
+        
+        default:
+            break;
+    }
+    return event;
 }
